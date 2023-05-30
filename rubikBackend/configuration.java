6cf5e508f531bd72a294ac672a46882fbea8ad6b
Jasper <114035580+Jaspvr@users.noreply.github.com>
2023-05-29 22:24:24 -0700

    Add files via upload

---
+++ rubikBackend/configuration.java
@@ -0,0 +1,226 @@
+//import java.util.* to import List, ArrayList, etc
+import java.util.*;
+
+class configuration {
+  //define each possible move, and the previous move made as integers
+  //there are only 6 possible moves on 2x2 because top moves are just the reverse of bottom moves, and etc 
+  static final int R = 0;
+  static final int Rprim = 1;
+  static final int U = 2;
+  static final int Uprim = 3;
+  static final int F = 4;
+  static final int Fprim = 5;
+  
+  int prevMove;
+
+  //now define an array to represent the colours on the cube
+  // 0 - white, 1 - red, 2 - orange, 3 - yellow, 4 - green, 5 - blue (although it wont matter for this file)
+  //use bytes to make the final calculation quicker
+  byte[] colours;
+
+  //we can now track both with a configuration object
+  configuration(byte[] arr, int move) {
+      this.colours = arr;
+      this.prevMove = move;
+  }
+
+    //type refers to which move is made (R, R', etc), colours is a configuration
+    static byte[] move(int type, byte[] colours) {
+
+        //creates an output array that represents a the new state of the cube after a specified move
+        byte[] coloursAfterMove = Arrays.copyOf(colours, colours.length);
+         
+        //match the type (these are both integer representations), and complete the move
+        if(type == R){
+         moveR(coloursAfterMove, colours);
+        }else if(type == Rprim){
+         moveRprim(coloursAfterMove, colours);
+        }if(type == U){
+         moveU(coloursAfterMove, colours);
+        }else if(type == Uprim){
+         moveUprim(coloursAfterMove, colours);
+        }if(type == F){
+         moveF(coloursAfterMove, colours);
+        }else if(type == Fprim){
+         moveFprim(coloursAfterMove, colours);
+        }
+  
+        //one of the moves has been made and the colours have been switched around
+        return coloursAfterMove;
+    }
+  
+  
+    //these methods actually create the configuration that would occur if the move was made, for each move
+   static void moveR(byte[] coloursAfterMove, byte[] colours) {
+    //new front
+    coloursAfterMove[2] = colours[22];
+    coloursAfterMove[4] = colours[24];
+    //new top
+    coloursAfterMove[18] = colours[2];
+    coloursAfterMove[20] = colours[4];
+    //new back
+    coloursAfterMove[9] = colours[20];
+    coloursAfterMove[11] = colours[18];
+    //new bottom
+    coloursAfterMove[22] = colours[11];
+    coloursAfterMove[24] = colours[9];
+    //new right
+     coloursAfterMove[5] = colours[7];
+     coloursAfterMove[6] = colours[5];
+     coloursAfterMove[7] = colours[8];
+     coloursAfterMove[8] = colours[6];
+ }
+  static void moveRprim(byte[] coloursAfterMove, byte[] colours) {
+    //new front
+    coloursAfterMove[2] = colours[18];
+    coloursAfterMove[4] = colours[20];
+    //new top
+    coloursAfterMove[18] = colours[11];
+    coloursAfterMove[20] = colours[9];
+    //new back
+    coloursAfterMove[11] = colours[22];
+    coloursAfterMove[9] = colours[24];
+    //new bottom
+    coloursAfterMove[22] = colours[2];
+    coloursAfterMove[24] = colours[4];
+    //new right
+    coloursAfterMove[8] = colours[7];
+    coloursAfterMove[7] = colours[5];
+    coloursAfterMove[5] = colours[6];
+    coloursAfterMove[6] = colours[8]; 
+ }
+  static void moveU(byte[] coloursAfterMove, byte[] colours) {
+    //new front
+    coloursAfterMove[1] = colours[5];
+    coloursAfterMove[2] = colours[6];
+    //new left
+    coloursAfterMove[13] = colours[1];
+    coloursAfterMove[14] = colours[2];
+    //new back
+    coloursAfterMove[9] = colours[13];
+    coloursAfterMove[10] = colours[14];
+    //new right
+    coloursAfterMove[5] = colours[9];
+    coloursAfterMove[6] = colours[10];
+    //new top
+    coloursAfterMove[17] = colours[19];
+    coloursAfterMove[18] = colours[17];
+    coloursAfterMove[19] = colours[20];
+    coloursAfterMove[20] = colours[18];  
+}
+
+
+static void moveUprim(byte[] coloursAfterMove, byte[] colours) {
+    //new front
+    coloursAfterMove[1] = colours[13];
+    coloursAfterMove[2] = colours[14];
+    //new right
+    coloursAfterMove[5] = colours[1];
+    coloursAfterMove[6] = colours[2];
+    //new back
+    coloursAfterMove[9] = colours[5];
+    coloursAfterMove[10] = colours[6];
+    //new left
+    coloursAfterMove[13] = colours[9];
+    coloursAfterMove[14] = colours[10];
+    //new top
+    coloursAfterMove[17] = colours[18];
+    coloursAfterMove[18] = colours[20];
+    coloursAfterMove[19] = colours[19];
+    coloursAfterMove[20] = colours[17];
+}
+  static void moveF(byte[] coloursAfterMove, byte[] colours) {
+    //new top
+    coloursAfterMove[19] = colours[16];
+    coloursAfterMove[20] = colours[14];
+    //new right
+    coloursAfterMove[5] = colours[19];
+    coloursAfterMove[7] = colours[20];
+    //new bottom
+    coloursAfterMove[22] = colours[5];
+    coloursAfterMove[21] = colours[7];
+    //new left
+    coloursAfterMove[14] = colours[21];
+    coloursAfterMove[16] = colours[22];
+    ///new front
+    coloursAfterMove[2] = colours[1];
+    coloursAfterMove[1] = colours[3];
+    coloursAfterMove[3] = colours[4];
+    coloursAfterMove[4] = colours[2];
+ }
+
+
+ static void moveFprim(byte[] coloursAfterMove, byte[] colours) {
+    //new top
+    coloursAfterMove[19] = colours[5];
+    coloursAfterMove[20] = colours[7];
+    //new right
+    coloursAfterMove[5] = colours[22];
+    coloursAfterMove[7] = colours[21];
+    //new bottom
+    coloursAfterMove[21] = colours[14];
+    coloursAfterMove[22] = colours[16];
+    //new left
+    coloursAfterMove[16] = colours[19];
+    coloursAfterMove[14] = colours[20];
+    //new front
+    coloursAfterMove[1] = colours[2];
+    coloursAfterMove[3] = colours[1];
+    coloursAfterMove[4] = colours[3];
+    coloursAfterMove[2] = colours[4];
+ }
+
+  //make a method to find all the cube configurations that are just one move away
+  //will be able to use this to find every possible configuration within a certain number of moves
+  //in knowing that a 2x2 cube can always be solved in 10 moves, if we exceed 12 we will know there is no possible solution.
+  List<configuration> oneAway() {
+      //initialize array list
+      List<configuration> oneAway = new ArrayList<>();
+
+      //there are only 6 possible moves as we have defined as integers above
+      //iterate through them all and add the new configuration that would occur if the move was made on the "current" configuration
+      int i = 0;
+       while(i<6){
+           byte[] oneAwayArray = move(i, colours);
+           configuration n = new configuration(oneAwayArray, i);
+           oneAway.add(n);
+           i++;
+       }
+
+      //return the arraylist (list of the numbers representing colours of all 6 configurations that are one away)
+      return oneAway;
+  }
+
+
+
+
+  //all these two override functions do is speed up the program
+ 
+  //overrides the default implementation of generating a hash code
+  @Override
+  public int hashCode() {
+     return Arrays.hashCode(colours);
+  }
+   //overrides the default implementation of object equality and checks if two states are the same
+  @Override
+  public boolean equals(Object o) {
+     if (this == o)
+          return true;
+      if (o == null || getClass() != o.getClass())
+          return false;
+      configuration coloursState = (configuration) o;
+      return Arrays.equals(colours, coloursState.colours);
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
--- rubikBackend/solve.java
+++ rubikBackend/solve.java
@@ -1,1 +1,183 @@
+//imports all classes and interfaces from the java.util package.
+//instead of just java.util.ArrayList; etc, java.util.* imports all util classes at once
+// things in util: linkedlist, ArrayList, HashSet, HashMap, PriorityQueue, Iterator, Comparable, TreeMap
+import java.util.*;
+
+//this class solves a rubiks cube by searching every possible cube configuration (using legal moves from the inital cube) until one is solved
+public class solve {
+
+   //array of strings representing the possible moves in the Rubik's cube
+   //R represents the clockwise rotation of the right face, whereas R' represents the counterclockwise rotation of the right face
+   static final String moves[] = new String[] { "R", "R'", "U", "U'", "F", "F'" };
+
+   //this method takes in a string which represents the initial state of the rubiks cube
+   //It then converts this string into a byte array to represent the colours on the cube
+   //Then each colour is assigned a number so it can be worked with easier
+   //The function will return an array numbers, so it could be called by a different method and then give back an easy array to use
+   //Once we connect to the html, this function is irrelevant because we will already have the array of colours
+   static byte[] convertScrambleToStickerArray(String scramble) {
+       byte[] cube = new byte[25];
+       for (int i = 1; i < cube.length; i++) {
+           switch (scramble.charAt(i - 1)) {
+           case 'w':
+               cube[i] = 0;
+               break;
+           case 'r':
+               cube[i] = 1;
+               break;
+           case 'o':
+               cube[i] = 2;
+               break;
+           case 'y':
+               cube[i] = 3;
+               break;
+           case 'g':
+               cube[i] = 4;
+               break;
+           case 'b':
+               cube[i] = 5;
+               break;
+
+
+           }
+       }
+       return cube;
+   }
+
+
+   //This method will check to see if the cube is solved or not
+   // It will take in the array of bytes and it will enter a for loop
+   // For loop iterates over every byte in the cube array, and compares its colour with the first colour of that face
+   // if the colour is different to the first of the face, it returns false, if it makes it all the way through it will return true
+   static boolean checkSolved(byte[] cube) {
+       int color = 0;
+       for (int i = 1; i < cube.length; i++) {
+           //this will set the colour to the first colour of the face (every 4 colours is a new face)
+           if (i % 4 == 1) {
+               color = cube[i];
+           }
+           //for the other three colours, if they arent the same as the first one then the function returns false
+           if (cube[i] != color) {
+               return false;
+           }
+       }
+       //if made it through the entire function then every face has been checked and all colours match appropriately
+       return true;
+   }
+
+
+   //the method will take in an object of the cube's configuration, and a Map object. It will return a list of strings representing the moves needed to solve the cube
+   static List<String> traceSteps(configuration c, Map<configuration, configuration> graph) {
+       //creates an empty list to be returned
+       List<String> steps = new ArrayList<>();
+
+       //while the state of the cube exists keep adding the last move from the cube's state
+       //then update the c variable using the map
+       while (c != null) {
+           steps.add(moves[c.prevMove]);
+           c = graph.get(c);
+       }
+       //remove original configuration
+       steps.remove(steps.size() - 1);
+       //must start from the scrambled state
+       Collections.reverse(steps);
+       return steps;
+   }
+
+
+   //This method will take in the state of the cube, and solve it using BFS
+   // Uses a HashMap to store previous states of the cube
+   // Starts the BFS from the configuration and continues until a solved state is found
+   // If a solved state is finally found, then calls trace steps method above to retrieve the list of moves
+   // With the returned steps of the tracesteps method, it can return this. If no solution then returns null
+   static List<String> solve(configuration configuration) {
+       //creaate hashmap, what are the two parameters again?
+       Map<configuration, configuration> prev = new HashMap<>();
+
+
+       //create queue of configurations
+       Queue<configuration> q = new ArrayDeque<>();
+
+
+       // add the current configuration to the queue
+       q.add(configuration);
+
+
+       //put the current cube state in the first part of map, there is no previous so its null
+       prev.put(configuration, null);
+
+
+       //now we can go through the queue
+       while (!q.isEmpty()) {
+           //poll() removes and returns the head of the q, could do this with pop()?
+           configuration curr = q.poll();
+
+           //check to see if the current state is solved, if it is then trace the steps and return that as the final solution
+           if (checkSolved(curr.colours)) {
+               return traceSteps(curr, prev);
+           }
+
+           //creates list of all the configurations one move away
+           List<configuration> oneAwayList = curr.oneAway();
+                for (int i = 0; i < oneAwayList.size(); i++) {
+                    configuration oneAway = oneAwayList.get(i);
+                    if (!prev.containsKey(oneAway)) {
+                        prev.put(oneAway, curr);
+                        q.add(oneAway);
+                    }
+                }
+       }
+       //no solution found, return null
+       return null;
+   }
+
+   //this main method will first give terminal output to the user
+   public static void main(String[] args) {
+       //scanner object called "in" reads input through the command line or console
+       //scanner is part of the java.util package which is imported at the top of the file
+       Scanner in = new Scanner(System.in);
+
+
+       //print out instructions to the user
+       //System.out.println("Enter 24 character string of sticker colors according to cube_diagram.png\n");
+       System.out.println("y-yellow");
+       System.out.println("r-red");
+       System.out.println("g-green");
+        System.out.println("b-blue");
+        System.out.println("w-white");
+        System.out.println("o-orange");
+/*
+ * 
+ * HERE WE WILL PARSE THE JSON FILE CONTAINING THE ARRAY OF COLOURS
+ * 
+ * 
+ * 
+ */
+
+       //this will read the user input as a single word then convert it to an array of numbers that represent colours
+       //we can void this from our program since we can get the client side to send over an array of numbers using javascript
+       String scramble = in.next();
+       byte[] cube = convertScrambleToStickerArray(scramble);
+
+
+       //starts timer
+       long tic = System.currentTimeMillis();
+
+
+       //initial cube state to feed into the solving algorithm
+       configuration init = new configuration(cube, 0);
+
+
+       //call solving algorithm on the configuration and set the output to a list of strings
+       List<String> solution = solve(init);
+
+
+       //print out the solution and time taken to solve it
+       System.out.println("Shortest solution:\n" + solution);
+       System.out.printf("Time taken to solve: %fs\n", (System.currentTimeMillis() - tic) / 1000f);
+
+
+   }
+}
+
 
